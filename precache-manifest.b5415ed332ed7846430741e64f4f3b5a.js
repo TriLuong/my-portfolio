8b31ceb0068e426070c58d82e4564be38da0eeb2
Tri Luong <triluongdl@gmail.com>
2020-01-12 19:33:18 +0700

    Updates

---
+++ precache-manifest.b5415ed332ed7846430741e64f4f3b5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ba20c4441e45ea6a4879eae2488ac91",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "e6a1198808dfd3057a86",
+    "url": "/my-portfolio/static/css/2.1c4011e7.chunk.css"
+  },
+  {
+    "revision": "1b8696f7b9c996ec289a",
+    "url": "/my-portfolio/static/css/main.899dd7bb.chunk.css"
+  },
+  {
+    "revision": "e6a1198808dfd3057a86",
+    "url": "/my-portfolio/static/js/2.b349e597.chunk.js"
+  },
+  {
+    "revision": "3a5da4106a08e596432e5e2628e1ef8d",
+    "url": "/my-portfolio/static/js/2.b349e597.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b8696f7b9c996ec289a",
+    "url": "/my-portfolio/static/js/main.049e24cb.chunk.js"
+  },
+  {
+    "revision": "4184a7eacb7c48bd5c06",
+    "url": "/my-portfolio/static/js/runtime-main.4a96037b.js"
+  },
+  {
+    "revision": "6b19b27b87776245d5cc607e92573f08",
+    "url": "/my-portfolio/static/media/currentLoads.6b19b27b.PNG"
+  },
+  {
+    "revision": "7b2e1d2d3513d6665f90775f276bc449",
+    "url": "/my-portfolio/static/media/dashboard.7b2e1d2d.PNG"
+  },
+  {
+    "revision": "8ac869dc2f5cacb4883c1c459ea97986",
+    "url": "/my-portfolio/static/media/driverApp.8ac869dc.PNG"
+  },
+  {
+    "revision": "c36e1c0ed13799acec629ba1cbb6f3ff",
+    "url": "/my-portfolio/static/media/loadHistory.c36e1c0e.PNG"
+  },
+  {
+    "revision": "30711a80e1c4d4753633edede7c84390",
+    "url": "/my-portfolio/static/media/users.30711a80.PNG"
+  }
+]);
