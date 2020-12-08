d2af94d51cf05b32573643f13bc59234e47bc6ad
Tri Luong <triluongdl@gmail.com>
2020-12-09 00:28:29 +0700

    Add project hubcv.

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,7 +3,7 @@ const rsLegendImage = {
   currentLoads: require("./rs-legend/currentLoads.PNG"),
   loadHistory: require("./rs-legend/loadHistory.PNG"),
   users: require("./rs-legend/users.PNG"),
-  driverApp: require("./rs-legend/driverApp.PNG")
+  driverApp: require("./rs-legend/driverApp.PNG"),
 };
 
 const skoocAppImage = {
@@ -13,7 +13,16 @@ const skoocAppImage = {
   skoocApp4: require("./skooc-app/skooc-app-4.png"),
   skoocApp5: require("./skooc-app/skooc-app-5.png"),
   skoocApp6: require("./skooc-app/skooc-app-6.png"),
-  skoocApp7: require("./skooc-app/skooc-app-7.png")
+  skoocApp7: require("./skooc-app/skooc-app-7.png"),
 };
 
-export default { rsLegendImage, skoocAppImage };
+const hubCvImage = {
+  hubCv1: require("./hubcv/hubcv_landing.png"),
+  hubCv2: require("./hubcv/hubcv_login.png"),
+  hubCv3: require("./hubcv/hubcv_home.png"),
+  hubCv4: require("./hubcv/hubcv_post.png"),
+  hubCv5: require("./hubcv/hubcv_profile.png"),
+  hubCv6: require("./hubcv/hubcv_chat.png"),
+};
+
+export default { rsLegendImage, skoocAppImage, hubCvImage };
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,34 +7,34 @@ export default [
       images.rsLegendImage.currentLoads,
       images.rsLegendImage.loadHistory,
       images.rsLegendImage.users,
-      images.rsLegendImage.driverApp
+      images.rsLegendImage.driverApp,
     ],
     title: "Legend Logistis",
     content:
       "Legend Trucking is a rapidly growing, asset-based trucking company. The intermodal transportation piece of the business utilizes disparate systems and manual processes that need to be automated for the business to scale.",
-    demos: [
-      {
-        src: "https://legend-staging.web.app/",
-        text: "CMS"
-      },
-      {
-        src: "https://legend-staging-driver.web.app/",
-        text: "Driver App"
-      }
-    ],
+    // demos: [
+    //   {
+    //     src: "https://legend-staging.web.app/",
+    //     text: "CMS",
+    //   },
+    //   {
+    //     src: "https://legend-staging-driver.web.app/",
+    //     text: "Driver App",
+    //   },
+    // ],
     features: [
       "Create and list all loads and users on CMS.",
       "Admin assigns Loads to specific driver, and notity to him through email and SMS.",
       "Update status of loads when driver picked up or dropped off them.",
-      "Admin can manage all loads and users on CMS."
+      "Admin can manage all loads and users on CMS.",
     ],
     technologies: [
       "This project is written by Reactjs with Gogo react admin tempalte.",
       "Use redux to manage state.",
       "Use twilio to send SMS.",
       "Use Landbot IO to create chatbot.",
-      "Use Algolia for fast search."
-    ]
+      "Use Algolia for fast search.",
+    ],
   },
 
   {
@@ -45,7 +45,7 @@ export default [
       images.skoocAppImage.skoocApp4,
       images.skoocAppImage.skoocApp5,
       images.skoocAppImage.skoocApp6,
-      images.skoocAppImage.skoocApp7
+      images.skoocAppImage.skoocApp7,
     ],
     title: "Skooc App",
     content:
@@ -62,12 +62,37 @@ export default [
     // ],
     features: [
       "User enter health index daily",
-      "Keep track of health index and make assessment daily."
+      "Keep track of health index and make assessment daily.",
     ],
     technologies: [
       "Use react-native to build app for IOS and Android devices",
       "Use redux to manage state.",
-      "Use AWS S3 to store images."
-    ]
-  }
+      "Use AWS S3 to store images.",
+    ],
+  },
+  {
+    photos: [
+      images.hubCvImage.hubCv1,
+      images.hubCvImage.hubCv2,
+      images.hubCvImage.hubCv3,
+      images.hubCvImage.hubCv4,
+      images.hubCvImage.hubCv5,
+      images.hubCvImage.hubCv6,
+    ],
+    title: "Hubcv",
+    content:
+      "Hubcv generates dynamic CV based on user's skill-based activities, projects they undertake, courses they learn and events they participate in.",
+    features: [
+      "Upload image, video, document and describe your skill",
+      "Users can connect and follow the others",
+      "Users can chat with people that they connected",
+      "The others can like, comment, share and bookmark on your skill.",
+      "University can create their course and students can enroll",
+    ],
+    technologies: [
+      "This project is written by ReactJs",
+      "Use redux, redux-saga to manage state.",
+      "Use socket-IO to show notification and implement chat",
+    ],
+  },
 ];
